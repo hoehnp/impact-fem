b52cd13c0a8c14b087c2d40518885f682b488bd7
yuriyyuriy <yuriyyuriy@e38c3513-6895-4f6e-a2e3-21dd2aee925f>
2014-02-22 18:59:35 +0000

    Replace Statistic & Java MicroBenchmark

--- Impact/make.bat
+++ Impact/make.bat
@@ -4,7 +4,7 @@ mkdir bin
 cd src
 
 rem javac -target 1.5 -cp .;%CLASSPATH%;..\lib\jogl.jar;..\lib\gluegen-rt.jar;..\lib\patbinfree153.jar -O -d ..\bin j3d\*.java run\*.java gui\*.java jp\lang\*.java jp\sync\*.java uka\transport\*.java uka\util\*.java uka\patch\*.java uka\karmi\rmi\*.java uka\karmi\rmi\server\*.java uka\karo\*.java
-javac -cp .;..\lib\jogl.jar;..\lib\gluegen-rt.jar;..\lib\patbinfree153.jar;..\lib_j3d\j3dcore.jar;..\lib_j3d\j3dutils.jar;..\lib_j3d\vecmath.jar;..\lib\LogDB.jar;..\lib\mysql-connector-java-5.1.11-bin.jar -O -d ..\bin run\*.java gui\*.java 
+javac -cp .;..\lib\jogl.jar;..\lib\gluegen-rt.jar;..\lib\patbinfree153.jar;..\lib_j3d\j3dcore.jar;..\lib_j3d\j3dutils.jar;..\lib_j3d\vecmath.jar -O -d ..\bin run\*.java gui\*.java 
 pause
 
 copy gui\*.gif ..\bin\gui\*.gif
